afeb833909bcc70e0fff91a03447c0c0df5af877
muzi <xiumuds@163.com>
2024-03-29 12:57:58 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="text-main">
+		<text class="iconfont icon-file-b-" style="font-size: 100rpx;"></text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
